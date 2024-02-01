83678a72b6c9294b176ea3e7e8d6fa761a7d6239
hcampos <hcampospro@gmail.com>
2024-02-01 23:58:59 +0100

    fix(selector.tsx): update selector button styles to match design requirements feat(selector.tsx): add text prop to Selector component to customize button text feat(selector.type.ts): add text prop to Props type for Selector component fix(line-up.section.tsx): add additional Selector components with different text values for filtering options

--- src/lib/components/ui/selector/selector.tsx
+++ src/lib/components/ui/selector/selector.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable max-len */
 import type { Component } from "@/lib/utils/component";
 import type { Props } from "./selector.type";
 
-export const Selector: Component<Props> = () => {
+export const Selector: Component<Props> = ({ text }) => {
   return (
     <button
       style={{ fontFamily: "Montserrat, sans-serif" }}
-      className="px-4 pt-[5px] pb-[3px] rounded-full border-2 border-blue"
+      className="px-5 pt-[5px] pb-[3px] rounded-[50px] border-2 border-blue font-semibold text-blue text-base hover:bg-blue hover:text-bodyBackground transition-all duration-300 ease-in-out"
     >
-      TOUS
+      {text}
     </button>
   );
 };
--- src/lib/sections/line-up.section.tsx
+++ src/lib/sections/line-up.section.tsx
@@ -6,10 +6,15 @@ import { Selector } from "../components/ui/selector";
 const LineUp = (): ReactElement => {
   return (
     <section className="w-screen h-dvh bg-lineup bg-no-repeat bg-cover bg-center px-60 py-28">
-      <h2 className="text-center text-5xl text-blue">LINE UP</h2>
+      <h2 className="text-center text-5xl text-blue mb-16">LINE UP</h2>
       <div className="w-full">
-        <div className="w-full flex items-center gap-3">
-          <Selector />
+        <div className="w-full flex items-center gap-3 mb-12">
+          <Selector text="TOUS" />
+          <Selector text="VENDREDI" />
+          <Selector text="SAMEDI" />
+        </div>
+        <div className="w-full bg-red-500">
+          Bonjour le monde je ne sais pas trop quoi dire de plus
         </div>
       </div>
     </section>
