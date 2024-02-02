aeeb352a0816dfcaf41f9e7024388e0ed08d4821
hcampos <hcampospro@gmail.com>
2024-02-02 14:30:56 +0100

    fix(artist-card.tsx): remove unused props and update styling for artist card component feat(artist-card.tsx): add social media icons to artist card component for Spotify, Youtube, Soundcloud, Instagram, Facebook, and Twitter feat(icons): add Facebook, Instagram, Soundcloud, Spotify, and Twitter icons feat(icons): add Youtube icon

--- src/lib/components/artist-card/artist-card.tsx
+++ src/lib/components/artist-card/artist-card.tsx
@@ -6,8 +6,14 @@ import type { Component } from "@/lib/utils/component";
 import type { Props } from "./artist-card.type";
 import Image from "next/image";
 import { useRef } from "react";
+import Spotify from "../icons/spotify";
+import Youtube from "../icons/youtube";
+import Soundcloud from "../icons/soundcloud";
+import Instagram from "../icons/instagram";
+import Facebook from "../icons/facebook";
+import Twitter from "../icons/twitter";
 
-export const ArtistCard: Component<Props> = ({ name, bio, day, imagePath, presenceDate }) => {
+export const ArtistCard: Component<Props> = ({ name, bio, imagePath }) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const infoRef = useRef<HTMLDivElement>(null);
 
@@ -48,15 +54,22 @@ export const ArtistCard: Component<Props> = ({ name, bio, day, imagePath, presen
           alt="Photo de l'artiste"
         />
       </div>
-      <div ref={infoRef} className="w-full opacity-0 rounded-sm bg-blue/70 bottom-0 p-4 transition-all absolute group-hover:opacity-1">
+      <div ref={infoRef} className="w-full opacity-0 rounded-md bg-blue/70 bottom-0 p-4 transition-all absolute group-hover:opacity-1">
         <h3 className="text-3xl text-bodyBackground tracking-wider mb-4">{name}</h3>
         <p
           style={{ fontFamily: "Montserrat, sans-serif" }}
-          className="text-sm w-full text-bodyBackground max-h-32 overflow-y-scroll no-scrollbar mb-4"
+          className="text-sm w-full text-bodyBackground max-h-32 overflow-y-scroll no-scrollbar mb-6"
         >
           {bio}
         </p>
-        <div className="w-full bg-red-400">Réseaux sociaux</div>
+        <div className="w-full flex items-center gap-4">
+          <Spotify className="text-bodyBackground" />
+          <Youtube className="text-bodyBackground" />
+          <Soundcloud className="text-bodyBackground" />
+          <Instagram className="text-bodyBackground" />
+          <Facebook className="text-bodyBackground" />
+          <Twitter className="text-bodyBackground" />
+        </div>
       </div>
     </div>
   );
---
+++ src/lib/components/icons/youtube.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+import { cn } from "@/lib/utils";
+import type { Component } from "@/lib/utils/component";
+
+type Props = {
+  className?: string;
+}
+
+const Youtube: Component<Props> = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={cn("icon icon-tabler icon-tabler-brand-youtube", className)} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" /><path d="M10 9l5 3l-5 3z" /></svg>
+  );
+};
+
+export default Youtube;
