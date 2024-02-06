252a83d83e39e62f2fb18b27431f8dcd611e06b9
hcampos <hcampospro@gmail.com>
2024-02-06 22:18:42 +0100

    feat: add lineup-content.config.ts file to store lineup content data
    The lineup-content.config.ts file is added to the src/config directory. This file contains an array of objects representing different sections of lineup content. Each object has a title and description property.
    
    The purpose of adding this file is to centralize the lineup content data and make it easier to manage and update. By separating the content data from the component code, it allows for easier maintenance and reusability of the content across different components.
    
    The content array in the story.section.tsx file is replaced with an import statement to import the content array from the lineup-content.config.ts file. This change ensures that the lineup content data is consistent across different sections that use it.
    
    This commit improves code organization and maintainability by separating the content data from the component code.

---
+++ src/config/lineup-content.config.ts
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+
+export const content = [
+  {
+    title: "Qui sommes nous ?",
+    description:
+      "Une bande copains"
+  },
+  {
+    title: "Real time changes",
+    description:
+      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates."
+  },
+  {
+    title: "Version control",
+    description:
+      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions."
+  },
+  {
+    title: "Running out of content",
+    description:
+      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions."
+  }
+];
