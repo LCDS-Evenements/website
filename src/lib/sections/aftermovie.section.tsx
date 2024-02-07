fb5c291b356c5350e23f7d6f83d7e924e55fd064
hcampos <hcampospro@gmail.com>
2024-02-07 11:57:08 +0100

    chore(aftermovie.section.tsx): add title and YouTube video player to AftermovieSection component for better user experience

--- src/lib/sections/aftermovie.section.tsx
+++ src/lib/sections/aftermovie.section.tsx
@@ -2,7 +2,18 @@ import type { ReactElement } from "react";
 
 const AftermovieSection = (): ReactElement => {
   return (
-    <section className="w-screen h-dvh bg-whiteWaves bg-no-repeat bg-cover bg-center"></section>
+    <section className="w-screen h-dvh bg-whiteWaves bg-no-repeat bg-cover bg-center py-28 px-60">
+      <h2 className="text-center text-5xl text-blue mb-16">AFTERMOVIE</h2>
+      <div className="w-full ">
+        <iframe
+          width="100%"
+          height="470"
+          src="https://www.youtube.com/embed/B23eCA_GXxU?si=ndkiMthBxff87Vvw"
+          title="YouTube video player"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        />
+      </div>
+    </section>
   );
 };
 
