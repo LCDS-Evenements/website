8067b68141d83ee5d4afdbe87685450003ee9fa4
hcampos <hcampospro@gmail.com>
2024-02-07 11:34:07 +0100

    chore(story.section.tsx): update margin-bottom value for h2 element in StorySection component to improve spacing

--- src/lib/sections/story.section.tsx
+++ src/lib/sections/story.section.tsx
@@ -6,7 +6,7 @@ import { content } from "@/config/lineup-content.config";
 const StorySection = (): ReactElement => {
   return (
     <section className="w-screen h-screen bg-blueWaves bg-no-repeat bg-cover bg-center py-28">
-      <h2 className="text-center text-5xl text-bodyBackground mb-16">NOTRE HISTOIRE</h2>
+      <h2 className="text-center text-5xl text-bodyBackground mb-24">NOTRE HISTOIRE</h2>
       <StickyScroll content={content} />
     </section>
   );
