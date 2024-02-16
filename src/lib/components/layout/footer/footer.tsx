5b260075563263f0092297779fd3182d9b7836e0
hcampos <hcampospro@gmail.com>
2024-02-16 16:02:43 +0100

    chore(footer.tsx): refactor footer component to improve readability and maintainability feat(footer.tsx): add logo image to the footer feat(footer.tsx): add email link to the footer feat(footer.tsx): add legal notice link to the footer feat(footer.tsx): add button for ticket sales to the footer feat(footer.tsx): update social media icons in the footer feat(footer.tsx): update design credits and copyright text in the footer

--- src/lib/components/layout/footer/footer.tsx
+++ src/lib/components/layout/footer/footer.tsx
@@ -2,35 +2,43 @@
 import type { ReactElement } from "react";
 import Instagram from "../../icons/instagram";
 import Facebook from "../../icons/facebook";
-import Tiktok from "../../icons/tiktok";
 import Youtube from "../../icons/youtube";
 import Link from "next/link";
+import Image from "next/image";
+import { ButtonLink } from "../../ui/button-link";
 
 export const Footer = (): ReactElement => {
   return (
-    <footer className="w-screen h-[45dvh] bg-blue py-16 px-28" style={{ fontFamily: "Montserrat, sans-serif" }}>
-      <div className="w-full h-full bg-red-500 flex flex-col justify-between items-center">
-        <div className="bg-orange-500 w-full h-[62%]">
-
+    <footer className="w-screen h-[45dvh] bg-whiteWaves bg-no-repeat bg-cover bg-center py-16 px-40">
+      <div className="w-full h-full flex flex-col justify-between items-center">
+        <div className="w-full h-[62%] flex items-center justify-between relative">
+          <div>
+            <Image src="./illustration/logo.svg" alt="Logo du festival LesCuicuitedays" width={120} height={120} />
+            <Link href="mailto:communication.cuicuitedays@gmail.com" style={{ fontFamily: "Montserrat, sans-serif" }} className="text-blue text-lg font-light">communication.cuicuitedays@gmail.com</Link>
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Link style={{ fontFamily: "Montserrat, sans-serif" }} className="text-blue text-lg font-light" href="/legal-notice">Mentions légales</Link>
+          </div>
+          <div>
+            <p style={{ fontFamily: "Montserrat, sans-serif" }} className="text-blue font-semibold text-xl mb-4">Prenez vos places !</p>
+            <ButtonLink href="" text="Billetterie" variant="hoverScaleYellow"/>
+          </div>
         </div>
-        <div className="w-full h-[2%] bg-divider"></div>
-        <div className="w-full h-[20%] bg-yellow-500 flex items-center justify-between relative">
+        <div className="w-full h-[1%] bg-divider"></div>
+        <div className="w-full h-[20%] flex items-center justify-between relative">
           <div className="flex items-center gap-4">
             <Link target="_blank" href="https://www.instagram.com/cuicuitedays/" className="hover:bg-divider p-3 rounded-full border-2 border-divider">
-              <Instagram className="text-bodyBackground w-5 h-5" />
+              <Instagram className="text-blue w-5 h-5" />
             </Link>
             <Link target='_blank' href="https://www.facebook.com/people/Les-Cuicuitedays/100089684250095/?mibextid=LQQJ4d" className="hover:bg-divider p-3 rounded-full border-2 border-divider">
-              <Facebook className="text-bodyBackground w-5 h-5" />
-            </Link>
-            <Link target='_blank' href="https://www.tiktok.com/@cuicuitedays" className="hover:bg-divider p-3 rounded-full border-2 border-divider">
-              <Tiktok className="text-bodyBackground w-5 h-5" />
+              <Facebook className="text-blue w-5 h-5" />
             </Link>
             <Link target='_blank' href="https://www.youtube.com/@lescuicuitedays" className="hover:bg-divider p-3 rounded-full border-2 border-divider">
-              <Youtube className="text-bodyBackground w-5 h-5" />
+              <Youtube className="text-blue w-5 h-5" />
             </Link>
           </div>
-          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-bodyBackground text-lg font-light">Design by Antoine GERVAIS and developped by Hugo CAMPOS</p>
-          <p className="text-lg font-light text-bodyBackground">© LesCuicuiteDays</p>
+          <p style={{ fontFamily: "Montserrat, sans-serif" }} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-blue text-lg font-light">Design by Antoine GERVAIS and developped by Hugo CAMPOS</p>
+          <p style={{ fontFamily: "Montserrat, sans-serif" }} className="text-lg font-light text-blue">© LesCuicuiteDays</p>
         </div>
       </div>
     </footer>
