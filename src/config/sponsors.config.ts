631c250d4359ab382d50b188d6c078f7b2a3a529
hcampos <hcampospro@gmail.com>
2024-02-16 15:14:04 +0100

    chore(package.json): add react-fast-marquee package to dependencies for marquee effect in sponsors section
    chore(sponsors.config.ts): update image paths for sponsors to use absolute paths instead of relative paths
    
    chore(sponsors.section.tsx): add marquee effect to display sponsors' logos in a scrolling manner

--- src/config/sponsors.config.ts
+++ src/config/sponsors.config.ts
@@ -7,47 +7,47 @@ export type Sponsor = {
 export const sponsors: Sponsor[] = [
   {
     name: "Bailly Chauffage logo",
-    imgPath: "./sponsors/bailly.jpeg",
+    imgPath: "/sponsors/bailly.jpeg",
     href: ""
   },
   {
     name: "Département des Deux-Sèvres 79 logo",
-    imgPath: "./sponsors/departement79.png",
+    imgPath: "/sponsors/departement79.png",
     href: "https://www.deux-sevres.fr/"
   },
   {
     name: "EGDC logo",
-    imgPath: "./sponsors/egdc.jpeg",
+    imgPath: "/sponsors/egdc.jpeg",
     href: "https://egdc.eu/"
   },
   {
     name: "OGS logo",
-    imgPath: "./sponsors/ogs.png",
+    imgPath: "/sponsors/ogs.png",
     href: "https://ogs-securite.com/"
   },
   {
     name: "Pavillon du Bocage logo",
-    imgPath: "./sponsors/pavillon.png",
+    imgPath: "/sponsors/pavillon.png",
     href: "https://www.lespavillonsdubocage.fr/"
   },
   {
     name: "Reveau Menuiserie logo",
-    imgPath: "./sponsors/reveau.png",
+    imgPath: "/sponsors/reveau.png",
     href: ""
   },
   {
     name: "V&B Bressuire logo",
-    imgPath: "./sponsors/v&b.png",
+    imgPath: "/sponsors/v&b.png",
     href: "https://www.vandb.fr/v-and-b-bressuire.html"
   },
   {
     name: "Xyleme logo",
-    imgPath: "./sponsors/xyleme.png",
+    imgPath: "/sponsors/xyleme.png",
     href: "https://www.xylemecom.fr/"
   },
   {
     name: "YC-Conseil logo",
-    imgPath: "./sponsors/yc-conseil.jpeg",
+    imgPath: "/sponsors/yc-conseil.jpeg",
     href: "https://www.yc-conseil.fr/"
   }
 ];
--- src/lib/sections/sponsors.section.tsx
+++ src/lib/sections/sponsors.section.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable max-len */
+import { sponsors } from "@/config/sponsors.config";
 import type { ReactElement } from "react";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+import Link from "next/link";
 
 const SponsorsSection = (): ReactElement => {
   return (
-    <section id="sponsors" className="w-screen h-[50dvh] bg-blueWaves bg-no-repeat bg-cover bg-center">
-      SponsorsSection
+    <section id="sponsors" className="w-screen h-[65dvh] bg-blueWaves bg-no-repeat bg-cover bg-center py-28 px-44">
+      <h2 className="text-center text-5xl text-bodyBackground mb-4">NOS SPONSORS</h2>
+      <p className="text-center text-xl text-bodyBackground mb-16">Ils nous font confiance...</p>
+      <div className="w-full display flex items-center justify-center">
+        <Marquee className="w-full" gradient autoFill speed={40} gradientColor="#1B2237" pauseOnHover>
+          {sponsors.map((sponsor, idx) => (
+            <Link href={sponsor.href} key={idx} className="bg-whiteWaves bg-bodyBackground bg-no-repeat bg-cover bg-center rounded-full w-full h-40 w-40 mx-6 flex items-center justify-center group">
+              <Image src={sponsor.imgPath} alt={sponsor.name} width={90} height={90} className="grayscale group-hover:grayscale-0" />
+            </Link>
+          ))}
+        </Marquee>
+      </div>
     </section>
   );
 };
