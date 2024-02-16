436686c96507ab19db3c8d2eb38b4f5b25d235f9
hcampos <hcampospro@gmail.com>
2024-02-16 12:23:45 +0100

    feat(sponsors.config.ts): add sponsors configuration file
    A new file `sponsors.config.ts` has been added to the `src/config` directory. This file contains the configuration for sponsors. It exports a `Sponsor` type and an array of `sponsors` objects. Each `sponsor` object has properties such as `name`, `imgPath`, and `href` which represent the name, image path, and hyperlink for a sponsor respectively.
    
    This configuration file is necessary to store and manage the information of sponsors for the application.

---
+++ src/config/sponsors.config.ts
@@ -0,0 +1,53 @@
+export type Sponsor = {
+  name: string;
+  imgPath: string;
+  href: string;
+}
+
+export const sponsors: Sponsor[] = [
+  {
+    name: "Bailly Chauffage logo",
+    imgPath: "./sponsors/bailly.jpeg",
+    href: ""
+  },
+  {
+    name: "Département des Deux-Sèvres 79 logo",
+    imgPath: "./sponsors/departement79.png",
+    href: "https://www.deux-sevres.fr/"
+  },
+  {
+    name: "EGDC logo",
+    imgPath: "./sponsors/egdc.jpeg",
+    href: "https://egdc.eu/"
+  },
+  {
+    name: "OGS logo",
+    imgPath: "./sponsors/ogs.png",
+    href: "https://ogs-securite.com/"
+  },
+  {
+    name: "Pavillon du Bocage logo",
+    imgPath: "./sponsors/pavillon.png",
+    href: "https://www.lespavillonsdubocage.fr/"
+  },
+  {
+    name: "Reveau Menuiserie logo",
+    imgPath: "./sponsors/reveau.png",
+    href: ""
+  },
+  {
+    name: "V&B Bressuire logo",
+    imgPath: "./sponsors/v&b.png",
+    href: "https://www.vandb.fr/v-and-b-bressuire.html"
+  },
+  {
+    name: "Xyleme logo",
+    imgPath: "./sponsors/xyleme.png",
+    href: "https://www.xylemecom.fr/"
+  },
+  {
+    name: "YC-Conseil logo",
+    imgPath: "./sponsors/yc-conseil.jpeg",
+    href: "https://www.yc-conseil.fr/"
+  }
+];
