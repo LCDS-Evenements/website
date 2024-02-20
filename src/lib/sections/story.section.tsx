0a962ed27f1874f2ae8b682652208582da54bca2
hcampos <hcampospro@gmail.com>
2024-02-20 19:14:49 +0100

    style(ui/sticky-scroll-reveal.tsx): update className to hide element on small screens and show on medium screens for better responsiveness style(sections/story.section.tsx): adjust styling for better layout on different screen sizes, including font sizes and content alignment

--- src/lib/sections/story.section.tsx
+++ src/lib/sections/story.section.tsx
@@ -5,9 +5,19 @@ import { content } from "@/config/lineup-content.config";
 
 const StorySection = (): ReactElement => {
   return (
-    <section id="story" className="w-screen h-screen bg-whiteWaves bg-no-repeat bg-cover bg-center py-28">
-      <h2 className="text-center text-5xl text-blue mb-24">NOTRE HISTOIRE</h2>
+    <section id="story" className="w-screen md:h-screen bg-whiteWaves bg-center bg-auto bg-repeat-space py-28">
+      <h2 className="text-center text-3xl md:text-5xl text-blue mb-16 md:mb-24">NOTRE HISTOIRE</h2>
       <StickyScroll content={content} />
+      <div className="w-[80%] mx-auto flex md:hidden flex-col justify-center gap-16">
+        {content.map((content, idx) => (
+          <div key={idx}>
+            <h4 className="text-blue text-2xl">{content.title}</h4>
+            <p className="text-base text-blue font-semibold tracking-wide max-w-lg mt-5" style={{ fontFamily: "Montserrat, sans-serif" }}>
+              {content.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
