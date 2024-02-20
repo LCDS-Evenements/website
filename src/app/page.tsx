1719882a75f0313ffafa321cbe9d65174c23f113
hcampos <hcampospro@gmail.com>
2024-02-20 01:15:50 +0100

    refactor(page.tsx): comment out unused imports to improve code readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import type { ReactElement } from "react";
 import HomeSection from "@/lib/sections/home.section";
 import LineUpSection from "@/lib/sections/line-up.section";
-import StorySection from "@/lib/sections/story.section";
-import AftermovieSection from "@/lib/sections/aftermovie.section";
-import SponsorsSection from "@/lib/sections/sponsors.section";
-import { Footer } from "@/lib/components/layout/footer";
+// import StorySection from "@/lib/sections/story.section";
+// import AftermovieSection from "@/lib/sections/aftermovie.section";
+// import SponsorsSection from "@/lib/sections/sponsors.section";
+// import { Footer } from "@/lib/components/layout/footer";
 
 const MainPage = (): ReactElement => {
   return (
