1d2847e5b209a7be3456eefffab4abc3955e2ed1
hcampos <hcampospro@gmail.com>
2024-02-20 12:46:09 +0100

    style(sponsors.section.tsx): add bottom margin to the Marquee component for better spacing

--- src/lib/sections/sponsors.section.tsx
+++ src/lib/sections/sponsors.section.tsx
@@ -11,7 +11,7 @@ const SponsorsSection = (): ReactElement => {
       <h2 className="text-center text-3xl md:text-5xl text-bodyBackground mb-4">NOS SPONSORS</h2>
       <p className="text-center text-lg md:text-xl text-bodyBackground mb-20 md:mb-16">Ils nous font confiance...</p>
       <div className="w-full display flex items-center justify-center">
-        <Marquee className="w-full" gradient autoFill speed={40} gradientColor="#1B2237" pauseOnHover>
+        <Marquee className="w-full mb-28" gradient autoFill speed={40} gradientColor="#1B2237" pauseOnHover>
           {sponsors.map((sponsor, idx) => (
             <Link target="_blank" href={sponsor.href} key={idx} className="bg-whiteWaves bg-bodyBackground bg-no-repeat bg-cover bg-center rounded-full h-40 w-40 mx-6 flex items-center justify-center group">
               <Image src={sponsor.imgPath} alt={sponsor.name} width={90} height={90} className="grayscale group-hover:grayscale-0" />
