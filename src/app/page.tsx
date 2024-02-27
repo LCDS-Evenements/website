39c90bf2ba784ddec45cb6da0a7e34e91005443a
hcampos <hcampospro@gmail.com>
2024-02-27 11:51:37 +0100

    refactor(page.tsx): optimize rendering logic by adding loading state and conditional rendering for better user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,41 @@
-import type { ReactElement } from "react";
+"use client";
+
+import { useEffect, useState, type ReactElement } from "react";
 import HomeSection from "@/lib/sections/home.section";
 import LineUpSection from "@/lib/sections/line-up.section";
 import SponsorsSection from "@/lib/sections/sponsors.section";
 import StorySection from "@/lib/sections/story.section";
 import AftermovieSection from "@/lib/sections/aftermovie.section";
+import Image from "next/image";
 
 const MainPage = (): ReactElement => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
 
   return (
     <main className="no-scrollbar w-screen">
-      <HomeSection />
-      <LineUpSection />
-      <SponsorsSection />
-      <StorySection />
-      <AftermovieSection />
+      {isLoaded && (
+        <>
+          <HomeSection />
+          <LineUpSection />
+          <SponsorsSection />
+          <StorySection />
+          <AftermovieSection />
+        </>
+      ) || (
+        <section className="h-screen w-screen flex items-center justify-center absolute z-50 bg-blueWaves bg-cover bg-center bg-no-repeat">
+          <Image
+            src="/illustration/white-logo.svg"
+            alt="Logo"
+            width={200}
+            height={200}
+            className="animate-pulse"
+          />
+        </section>
+      )}
     </main>
   );
 };
