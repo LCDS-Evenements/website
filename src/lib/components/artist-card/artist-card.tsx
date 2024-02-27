f57827daff1bcddb1938fbb460de8c78dffe2d1d
hcampos <hcampospro@gmail.com>
2024-02-27 11:40:51 +0100

    refactor(artist-card.tsx): remove loading attribute from image tag in ArtistCard component to improve code cleanliness and consistency

--- src/lib/components/artist-card/artist-card.tsx
+++ src/lib/components/artist-card/artist-card.tsx
@@ -75,7 +75,6 @@ export const ArtistCard: Component<Props> = ({
           width={100}
           height={100}
           alt="Photo de l'artiste"
-          loading="lazy"
         />
       </div>
       <div ref={infoRef} className="w-full opacity-0 rounded-md bg-blue/70 bottom-0 p-4 transition-all absolute group-hover:opacity-1">
