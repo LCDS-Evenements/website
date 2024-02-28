6b308cca79b0f1c80e9038c7f25dff4b964a6a7c
hcampos <hcampospro@gmail.com>
2024-02-28 12:53:40 +0100

    style(stories): remove unused CSS files and update existing CSS styles to improve code cleanliness and maintainability

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,31 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
-  },
-};
+
+    backgrounds: {
+      default: "black",
+      values: [
+        {
+          name: "black",
+          value: "#0c0c0d",
+        },
+        {
+          name: "white",
+          value: "#FFFFFF",
+        }
+      ],
+    }
+  }
+}
 
 export default preview;
