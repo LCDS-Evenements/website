9f0e50d89db7797edfb0207efa879944042ea9f5
hcampos <hcampospro@gmail.com>
2024-03-01 17:39:50 +0100

    fix(story-content.config.ts): correct grammar in description for better readability and consistency

--- src/config/story-content.config.ts
+++ src/config/story-content.config.ts
@@ -9,14 +9,14 @@ export const content: Content[] = [
   {
     title: "Qui sommes nous ?",
     description: [
-      "Nous sommes une bande de copains, qui ce sont lancés le pari d'organiser un évenement qui réunis pleins de personnes afin de passer un bon moment tous ensemble 🤩.",
+      "Nous sommes une bande de copains, qui se sont lancé le pari d'organiser un évenement qui réunit plein de personnes afin de passer un bon moment tous ensemble 🤩.",
       "Pari réussi, car pour cette nouvelle édition le festival ouvre ses portes à encore plus de monde 🫶 !"
     ]
   },
   {
     title: "Les CuicuiteDays c'est quoi ?",
     description: [
-      "Les CuicuiteDays c'est un festival, qui est maintenant rendu à ca deuxième édition ouverte au publique 🌟.",
+      "Les CuicuiteDays, c'est un festival, qui est maintenant rendu à sa deuxième édition ouverte au publique 🌟.",
       "Il dure deux jours, réunit une dizaine d'artiste, des activités, des food-trucks, beaucoup de monde et surtout une ambiance de fou 🎉 !"
     ]
   },
