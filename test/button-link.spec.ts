df0939c5bcb3d872f07b2f8f3d0142e81ce70b2d
hcampos <hcampospro@gmail.com>
2024-03-15 11:50:55 +0100

    chore(button-link.spec.ts): delete outdated button-link.spec.ts file feat(button-link.spec.ts): create new button-link.spec.ts file with updated Playwright tests for button links in different sections of the website

---
+++ test/button-link.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from "@playwright/test";
+import { billetterieLink } from "@/config/billetterie.config";
+
+test.describe("ButtonLink", ()  => {
+  test("button link in the navbar should go to the shop", async({ page, context }) => {
+    await page.goto("/");
+
+    const billetterieButton = page.getByTestId("billetterie-button-1");
+
+    await billetterieButton.click();
+
+    const pagePromise = context.waitForEvent("page");
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage.url()).toBe(billetterieLink);
+  });
+
+  test("button link in the home section should go to the shop", async({ page, context }) => {
+    await page.goto("/");
+
+    const billetterieButton = page.getByTestId("billetterie-button-2");
+
+    await billetterieButton.click();
+
+    const pagePromise = context.waitForEvent("page");
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage.url()).toBe(billetterieLink);
+  });
+
+  test("button link in the footer should go to the shop", async({ page, context }) => {
+    await page.goto("/");
+
+    const billetterieButton = page.getByTestId("billetterie-button-3");
+
+    await billetterieButton.click();
+
+    const pagePromise = context.waitForEvent("page");
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage.url()).toBe(billetterieLink);
+  });
+});
