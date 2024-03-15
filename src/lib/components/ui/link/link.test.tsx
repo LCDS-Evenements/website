efc974fd6a88159756d88b068296dce4459bc2f0
hcampos <hcampospro@gmail.com>
2024-03-15 11:13:43 +0100

    test(link.test.tsx): update test description to accurately reflect the test functionality

--- src/lib/components/ui/link/link.test.tsx
+++ src/lib/components/ui/link/link.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/experimental-ct-react";
 import { CustomLink } from ".";
 
 test.describe("UI/CustomLink", () => {
-  test("should render the component", async({ mount, page }) => {
+  test("should go to the right page", async({ mount, page }) => {
     const component = await mount(
       <CustomLink href="https://cuicuitedays.fr" text="CustomLink" />
     );
