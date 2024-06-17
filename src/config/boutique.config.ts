4b9c1e3407039bbdfcb1d096502819ed871a4bad
hcampos <hcampospro@gmail.com>
2024-06-17 21:04:37 +0200

    fix(boutique.config.ts): update merchLink URL to correct link for boutique merch shop

--- src/config/boutique.config.ts
+++ src/config/boutique.config.ts
@@ -1,1 +1,1 @@
-export const merchLink = "https://www.helloasso.com/associations/lcds-evenements/boutiques/boutique-merch-pour-le-festival-lescuicuitedays";
+export const merchLink = "https://www.helloasso.com/associations/lcds-evenements/boutiques/boutique-merch-pour-le-festival-lescuicuitedays-1";
